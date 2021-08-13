bfc728d8dc81b2307d79fd5b6a321ad29942254e
febg-codecov <felipe@codecov.io>
2021-08-13 11:15:34 -0500

    more funcs

--- src/index2.py
+++ src/index2.py
@@ -1,3 +1,16 @@
+def uncovered_if2(var=True):
+    if var:
+      return False
+    else:
+      return True
+
+def fully_covered2():
+    # Added a change here
+    return True
+
+def uncovered2():
+    return True
+
 def uncovered_if(var=True):
     if var:
       return False
